1ccb5b14bc1c464034d8f6477ab31903bb389946
Haga-kure <dd4184968@gmail.com>
2023-05-11 14:37:47 +0800

    Forget v1

--- client/src/pages/ForgetModal.tsx
+++ client/src/pages/ForgetModal.tsx
@@ -13,17 +13,15 @@ import "react-toastify/dist/ReactToastify.css";
 
 import axios from "axios";
 
-export default function SigninModal() {
+export default function ForgetModal() {
   const [isOpen, setIsOpen] = useState(true);
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const router = useRouter();
 
   const submitSignin = () => {
     axios
       .post("http://localhost:7777/signin", {
         email,
-        password,
       })
       .then((data) => {
         localStorage.setItem("token", data.data.token);
@@ -104,23 +102,17 @@ export default function SigninModal() {
                     >
                       <Dialog.Title
                         as="h3"
-                        className="text-lg text-center font-bold leading-6 text-gray-900"
+                        className="text-lg font-bold leading-6 text-gray-900 mb-4"
                       >
-                        Welcome Back !
+                        Find your account
                       </Dialog.Title>
                       <div>
-                        <label
-                          htmlFor="email"
-                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        >
-                          Your email
-                        </label>
                         <input
                           value={email}
                           type="email"
                           name="email"
                           id="email"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          className="bg-gray-50 border mb-2 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           placeholder="Email address"
                           onChange={(e) => {
                             setEmail(e.target.value);
@@ -128,61 +120,9 @@ export default function SigninModal() {
                           required
                         />
                       </div>
-                      <div className="my-3">
-                        <label
-                          htmlFor="password"
-                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        >
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          name="password"
-                          id="password"
-                          placeholder="Password"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                          onChange={(e) => {
-                            setPassword(e.target.value);
-                          }}
-                          required
-                        />
-                      </div>
-                      <div className="flex">
-                        <div className="flex items-center my-3 h-5">
-                          <input
-                            id="terms"
-                            aria-describedby="terms"
-                            type="checkbox"
-                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                            required
-                          />
-                        </div>
-                        <div className="ml-3 my-3 text-sm d-flex">
-                          <label
-                            htmlFor="terms"
-                            className="font-light text-gray-500 dark:text-gray-300"
-                          >
-                            I accept the{" "}
-                            <a
-                              className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                              href="#"
-                            >
-                              Terms and Conditions
-                            </a>
-                          </label>
-                        </div>
-                        <div className="flex items-center ms-40">
-                          <a
-                            href="/ForgetModal"
-                            className="font-lg text-gray-500 dark:text-gray-300"
-                          >
-                            Forget Password?
-                          </a>
-                        </div>
-                      </div>
                       <button
                         type="submit"
-                        className="btn w-full h-full bg-o rounded-lg"
+                        className="btn w-full h-full bg-o rounded-full"
                       >
                         Continue
                       </button>
