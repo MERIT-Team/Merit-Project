7ca9f4c3a6eb417c8db61fda7810386574937ee3
Haga-kure <dd4184968@gmail.com>
2023-05-17 16:36:09 +0800

    Footer Final Version

--- server/src/country_codes/country_codes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CountryCodesService } from './country_codes.service';
-import { CountryCodesController } from './country_codes.controller';
-
-@Module({
-  controllers: [CountryCodesController],
-  providers: [CountryCodesService]
-})
-export class CountryCodesModule {}
--- server/src/country_codes/dto/update-country_code.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCountryCodeDto } from './create-country_code.dto';
-
-export class UpdateCountryCodeDto extends PartialType(CreateCountryCodeDto) {}
